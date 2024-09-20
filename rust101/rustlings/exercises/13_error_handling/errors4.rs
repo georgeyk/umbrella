20b32b3453c9b233aa64a8f154aa5feed579a889
George Y. Kussumoto <georgeyk.dev@gmail.com>
2024-09-20 17:06:29 -0300

    rust101 - session#6

--- rust101/rustlings/exercises/13_error_handling/errors4.rs
+++ rust101/rustlings/exercises/13_error_handling/errors4.rs
@@ -9,8 +9,11 @@ struct PositiveNonzeroInteger(u64);
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<Self, CreationError> {
-        // TODO: This function shouldn't always return an `Ok`.
-        Ok(Self(value as u64))
+        match value {
+            x if x < 0 => Err(CreationError::Negative),
+            0 => Err(CreationError::Zero),
+            x => Ok(PositiveNonzeroInteger(x as u64)),
+        }
     }
 }
 
